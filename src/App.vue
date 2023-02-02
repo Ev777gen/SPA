620ece2d7a2ca288d99ed20de48cc59aa79ce0aa
Ev777gen <fortheartist@mail.ru>
2023-02-02 19:45:28 +0500

    created project. added basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <TheNavbar/>
+  <div class="container">
+    <div class="sidebar">Sidebar</div>
+    <div class="content">
+      Content
+      <router-view/>
+    </div>
+  </div>
 </template>
 
+<script>
+import TheNavbar from '@/components/TheNavbar'
+export default {
+  name: 'App',
+  components: { TheNavbar },
+  data () {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
 <style lang="scss">
+@import '@/assets/reset.css';
+
+$max-width: 1100px;
+$content-width: 900px;
+
+html,
+body {
+  min-height: 100vh;
+  background-color: #fff;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #1a1a1a;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  display: flex;
+  justify-content: space-between;
+  max-width: $max-width;
+  margin: 0px auto;
+  padding: 0 15px;
+  border: 1px solid red;
+}
+.sidebar {
+  width: $max-width - $content-width;
+  border: 1px solid blue;
+}
+.content {
+  width: $content-width;
+  border: 1px solid blue;
 }
 </style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header" id="header">
+    <div class="container">
+      <router-link :to="{name: 'Home'}" class="header__link"><span class="header__logo">Logo</span></router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  padding: 20px 0;
+  background-color: #23374d;
+}
+.header__logo {
+  font-size: 24px;
+  font-weight: bold;
+}
+.header__link {
+  color: #fff;
+}
+.nav {
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
