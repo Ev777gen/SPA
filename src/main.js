5773ed48263a421cb29a14776eaab221d4743ec8
Ev777gen <fortheartist@mail.ru>
2023-02-08 11:30:54 +0500

    добавил в main.js регистрацию базовых компонентов

--- src/main.js
+++ src/main.js
@@ -3,4 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+  .use(store)
+  .use(router)
+
+// Базовые компоненты: делаем глобальными компоненты, начинающиеся на App...
+const requireComponent = require.context("@/components", true, /App[A-Z]\w+\.(vue|js)$/)
+requireComponent.keys().forEach(function (fileName) {
+  let baseComponentConfig = requireComponent(fileName)
+  baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+  const baseComponentName = baseComponentConfig.name || (
+    fileName
+      .replace(/^.+\//, '')
+      .replace(/\.\w+$/, '')
+  )
+  app.component(baseComponentName, baseComponentConfig)
+})
+
+app.mount('#app')
